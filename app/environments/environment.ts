4858db785a69fd2613d71ff17077f78cf40cd0b6
antonioDiaz04 <20221136@uthh.edu.mx>
2025-11-24 10:57:25 -0600

    url back https

--- app/environments/environment.ts
+++ app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  api: 'http://proyecto-atr-backend-yq3e.onrender.com/api/v1', //!alojado
+  api: 'https://proyecto-atr-backend-yq3e.onrender.com/api/v1', //!alojado
 };
 // https://proyecto-atr-backend-l06t.onrender.com/api/v1
